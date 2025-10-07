7a333f6803a9fcf43c91c6f6a1afa37eb253f1a9
mariohamann <mario.hamann@virtual-identity.com>
2025-10-07 14:02:13 +0200

    add image compression

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite';
+import vsharp from 'vite-plugin-vsharp';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [vue(), tailwindcss(), vsharp({
+    width: 1000,
+    height: 1000,
+    preserveMetadata: {
+      orientation: true,
+    },
+    ".jpg": {
+      "quality": 80
+    },
+  }),],
   base: '/sozialwerk-breisgau-job-quiz'
 });
